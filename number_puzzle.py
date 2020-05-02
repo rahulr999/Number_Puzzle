fb52b3e1178758b6128f01d9db4b786c575ba34b
rahulr999 <32889137+rahulr999@users.noreply.github.com>
2020-05-02 10:24:23 -0400

    Update number_puzzle.py

--- number_puzzle.py
+++ number_puzzle.py
@@ -1,31 +1,35 @@
 import copy
-array = [[1,4,2],[6,3,5],[0,7,8]]
-#array = [array[2],array[0],array[1]]
-finish = 0
-list_array = []
-prev_array = []
-for x in range(len(array)):
-    for y in range(len(array)):
+array = [[1,4,2],[6,3,5],[0,7,8]] #array for the input here 0 is assumed to be  "-"
+finish = 0 # Flag to check is a solution has been reached
+list_array = [] #Storing the list of currently explored states
+prev_array = [] #Stroing the list of possible places to prevent recuring tiles to avoid looping
+sol = [] # the actual solution
+cnt = 0
+for x in range(len(array)):   #Initalising the values of index value of the swap tile and the actual solution
+    sl = []
+    for y in range(len(array[0])):
+        sl.append(cnt)
+        cnt+=1
         if array[x][y] == 0:
             i=x
             j=y
-            break
-def heuristic_val(array):
-    sol = [[0,1,2],[3,4,5],[6,7,8]]
+    sol.append(sl)
+def heuristic_val(array):  #Calculates the heuristic for a given state and to check if the solution has been met
+    global sol
     val = 0
     for i in range(len(array)):
         for j in range(len(array[1])):
             if array[i][j] != sol[i][j] and array[i][j] != 0:
                 val += abs(array[i][j] - ((3*i)+j))
     return val
-def heuristic_fun(list_array):
+def heuristic_fun(list_array): #to explore the new states
     global finish,prev_array
     while finish == 0 or len(list_array) > 0:
         array = list_array[0]
         del list_array[0]
         i = array[0][0]
         j = array[0][1]
-        if i < 2:
+        if i < len(sol)-1: #
             arr = copy.deepcopy(array[1])
             temp = arr[i][j]
             arr[i][j] = arr[i+1][j]
@@ -41,7 +45,7 @@ def heuristic_fun(list_array):
             if arr not in prev_array:
                 prev_array.append(arr)
                 list_array.append([[i-1,j],arr,array[2]+["Up"],heuristic_val(arr)+(len(array[2])+1)*3])
-        if j < 2:
+        if j < len(sol[0])-1:
             arr = copy.deepcopy(array[1])
             temp = arr[i][j]
             arr[i][j] = arr[i][j+1]
@@ -54,16 +58,15 @@ def heuristic_fun(list_array):
             temp = arr[i][j]
             arr[i][j] = arr[i][j-1]
             arr[i][j-1] = temp
-            if arr not in prev_array:
+            if arr not in prev_array: #To check if this state has been reached before
                 prev_array.append(arr)
                 list_array.append([[i,j-1],arr,array[2]+["Left"],heuristic_val(arr)+(len(array[2])+1)*3])
-        list_array.sort(key = lambda x: x[3])
-        print(list_array[0])
-        if heuristic_val(list_array[0][1]) == 0:
+        list_array.sort(key = lambda x: x[3]) #Sorting the list based on the heuristic value
+        if heuristic_val(list_array[0][1]) == 0: #To check if the solution has been reached
             finish = 1
             break
     return list_array[0][2]
-def print_mat(array):
+def print_mat(array): #printing the list of steps that leades to the solution
     for x in array:
         st = ""
         for y in x:
@@ -71,7 +74,7 @@ def print_mat(array):
             else: st+=str(y)+" "
         print(st)
     print("\n")
-move = heuristic_fun([[[i,j],array,[],9999]])
+move = heuristic_fun([[[i,j],array,[],9999]]) #calling the heuristic function
 print_mat(array)
 
 for mv in move:
